807a2fd59f137806ea57a048fa0706c297167d0f
Wilson Rondini <wilbecoding@gmail.com>
2015-08-07 13:50:01 -0600

    My near solution w/class example

--- house.js
+++ house.js
@@ -1,5 +1,23 @@
-function House(numberOfDoors) {
-  this.numberOfDoors = numberOfDoors
-}
 
+function House(numberOfDoors) {
+  this.numberOfDoors = numberOfDoors;
+  this.numberOfWindows = numberOfWindows;
+  // PLURALIZATION BRAH
+// Put the WHOLE RETURN STATEMENT in the If Else Statement
+ this.toString = function() {
+    var doors;
+    var windows;
+    if(this.numberOfDoors === 0 || this.numberOfDoors > 1) {
+      var doors = 'doors';
+    } else {
+      var doors = 'door';
+    }
+    if(this.numberOfWindows === 0 || this.numberOfWindows > 1) {
+      var windows = 'windows';
+    } else {
+      var windows = 'window';
+    }
+    return ("The house has " + this.numberOfDoors + " " +  doors + " and " + this.numberOfWindows + " " + windows);
+}
+}
 module.exports = House;
---
+++ houseSolutions.js
@@ -0,0 +1,23 @@
+// Bills Solution
+
+function House(numberOfDoors, numberOfWindows) {
+    this.numberOfDoors = numberOfDoors;
+    this.numberOfWindows = numberOfWindows;
+    this.toString = function () {
+        if (numberOfDoors > 1 && numberOfWindows === 1) {
+            return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " window";
+        } else if (numberOfDoors > 1 && numberOfWindows > 1) {
+            return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " windows";
+        } else if (numberOfDoors === 1 && numberOfWindows > 1) {
+            return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " windows";
+        } else if (numberOfDoors === 1 && numberOfWindows === 1) {
+            return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " window";
+        } else if (numberOfDoors < 1 && numberOfWindows < 1) {
+            return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " windows";
+        }
+    }
+
+}
+
+module.exports = House;
+
